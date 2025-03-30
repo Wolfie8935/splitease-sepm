50d30d035826cd33599d71d0bd3b5158439a54d3
Aman Goel <124771341+Wolfie8935@users.noreply.github.com>
2025-03-30 21:41:59 +0530

    bugs fix more

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <footer className="bg-background border-t mt-auto py-6">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
-        {/* <p className="text-sm text-muted-foreground text-center mb-2 md:mb-0">
-          &copy; {new Date().getFullYear()} SplitEase. All rights reserved.
-        </p> */}
+        <p className="text-sm text-muted-foreground text-center mb-2 md:mb-0">
+                                      &copy; {new Date().getFullYear()} SplitEase. All rights reserved.
+        </p>
         <p className="text-sm text-muted-foreground text-center">
         Created by Aman Goel, Rishaan Yadav and Jayanth Nair
         </p>
